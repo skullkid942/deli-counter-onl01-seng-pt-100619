6057b3ba770f259f9a27fcdd11ae494c39e8c65f
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-16 14:41:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def line 
+  katz_deli = []
+  if katz_deli.size = 0 
+    puts "The line is currently empty."
+  else
+    while katz_deli[counter] do
+      puts katz_deli[counter]
+      counter += 1 
+    end 
+end
